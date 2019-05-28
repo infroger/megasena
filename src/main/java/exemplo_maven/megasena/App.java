4282d0542d54521856a583325971622252ca030c
Roger <roger.krolow@gmail.com>
2019-05-28 02:50:35 -0300

    Commit inicial

---
+++ src/main/java/exemplo_maven/megasena/App.java
@@ -0,0 +1,22 @@
+package exemplo_maven.megasena;
+import java.util.Arrays;
+
+public class App 
+{
+	public static int getNumero( ) {
+		return (int) (Math.random() * 60 + 1);
+	}
+
+	public static int[] getJogo( ) {
+		int[] jogo = new int[6];
+    	for (int i=0; i<10; i++)
+    		jogo[i] = getNumero();
+    	return jogo;
+	}
+
+	public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+  		System.out.println("Dezenas: " +Arrays.toString(getJogo()));
+    }
+}
---
+++ src/test/java/exemplo_maven/megasena/AppTest.java
@@ -0,0 +1,48 @@
+package exemplo_maven.megasena;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    public void testNumero()
+    {
+        assertTrue( App.getNumero() <= 60 );
+    }
+
+    public void testJogo()
+    {
+        assertTrue( App.getJogo().length == 6 );
+    }       
+}
